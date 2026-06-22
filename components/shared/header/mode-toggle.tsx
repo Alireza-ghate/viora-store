2cb45b09283becaf689ec0079d14cf83b62bc47b
Alireza-ghate <alireza.ghate.jd@gmail.com>
2026-06-22 22:19:48 +0330

    Add dark/light mode functionality

---
+++ components/shared/header/mode-toggle.tsx
@@ -0,0 +1,71 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuCheckboxItem,
+  DropdownMenuTrigger,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+} from "@/components/ui/dropdown-menu";
+import { SunIcon, MoonIcon, SunMoon } from "lucide-react";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+
+function ModeToggle() {
+  const { theme, setTheme } = useTheme();
+
+  // for fix hydration error:
+  // 1) suppressHydrationWarning prop dosnt enough
+  // 2) we must make sure this component mounts before uses theme SO we need useffect in first mount
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => {
+    // eslint-disable-next-line react-hooks/set-state-in-effect
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null; // if component dosnt mount b4 uses theme returns nothing
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          className="focus-visible:ring-0 focus-visible:ring-offset-0"
+          variant={"ghost"}
+        >
+          {theme === "light" ? (
+            <SunIcon />
+          ) : theme === "dark" ? (
+            <MoonIcon />
+          ) : (
+            <SunMoon />
+          )}
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-32" align="start">
+        <DropdownMenuLabel>Appearance</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuCheckboxItem
+          checked={theme === "light"}
+          onClick={() => setTheme("light")}
+        >
+          Light
+        </DropdownMenuCheckboxItem>
+        <DropdownMenuCheckboxItem
+          checked={theme === "dark"}
+          onClick={() => setTheme("dark")}
+        >
+          Dark
+        </DropdownMenuCheckboxItem>
+        <DropdownMenuCheckboxItem
+          checked={theme === "system"}
+          onClick={() => setTheme("system")}
+        >
+          System
+        </DropdownMenuCheckboxItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
+
+export default ModeToggle;
